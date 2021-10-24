e3dcc1e7e3c7dd5a650aecee18f2b2a886df18e4
dmitriyl99 <dmitriyl1899@gmail.com>
2021-10-24 07:47:28 +0500

    format response from data from rec sys

--- bookly_api/app/api/endpoints/recommendation.py
+++ bookly_api/app/api/endpoints/recommendation.py
@@ -1,27 +1,44 @@
+from typing import Optional
+
 from fastapi import APIRouter, HTTPException, Depends
 
-from app.data.repositories.readers_repository import ReadersRepository
+from app.data.repositories import BooksRepository
+from app.core.recommendation import RecommendationSystem
 
-from app.api.deps import get_readers_repository
+from app.api.deps import get_recommendation_service, get_books_repository
 
 
 router = APIRouter()
 
 
 @router.get('/recommendation')
-async def get_recommendation_for_reader(reader_id: int,
-                                        readers_repository: ReadersRepository = Depends(get_readers_repository)):
+async def get_recommendation_for_reader(reader_id: Optional[int] = None,
+                                        books_repository: BooksRepository = Depends(get_books_repository),
+                                        rec_service: RecommendationSystem = Depends(get_recommendation_service)):
     """
     Get recommendations for reader
     """
-    if reader_id is None:
-        raise HTTPException(
-            status_code=400,
-            detail='Provide reader id'
-        )
-    reader = readers_repository.get_reader_by_id(reader_id=reader_id)
-    if reader is None:
-        raise HTTPException(
-            status_code=404,
-            detail='Reader not found'
-        )
+    recommendations, history = rec_service.get_recommendations_for_reader(reader_id=reader_id)
+    recommendations_response = []
+    history_response = []
+    recommendation_books = books_repository.get_books_by_ids(recommendations)
+    history_books = books_repository.get_books_by_ids(history)
+    for book in recommendation_books:
+        recommendations_response.append({
+            'id': book[0],
+            'title': book[1],
+            'author': book[2]
+        })
+    for book in history_books:
+        history_response.append({
+            'id': book[0],
+            'title': book[1],
+            'author': book[2]
+        })
+
+    return {
+        'data': {
+            'recommendations': recommendations_response,
+            'history': history_response
+        }
+    }
