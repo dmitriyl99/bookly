369d909a18a69ae0f349e52b1aa832d504807b61
dmitriyl99 <dmitriyl1899@gmail.com>
2021-10-24 07:46:45 +0500

    Get books with id, title and author by list of ids

--- bookly_api/app/data/repositories/books_repository.py
+++ bookly_api/app/data/repositories/books_repository.py
@@ -1,8 +1,8 @@
-from typing import List, Generator
+from typing import List, Optional, Tuple
 
 from sqlalchemy.orm import Session
 
-from app.data.tables import books
+from app.data.tables import books, authors
 
 
 class BooksRepository:
@@ -15,3 +15,12 @@ class BooksRepository:
     def get_all_books_ids(self) -> List[int]:
         result = self.db.query(books.columns.id).order_by(books.columns.id).all()
         return [i[0] for i in result]
+
+    def get_book_by_id(self, book_id: int) -> Optional[Tuple]:
+        return self.db.query(books).get(book_id)
+
+    def get_books_by_ids(self, book_ids: list) -> List[Tuple]:
+        return self.db.query(books.columns.id, books.columns.title, authors.columns.name)\
+            .join(authors, books.columns.author_id == authors.columns.id, isouter=True)\
+            .filter(books.columns.id.in_(tuple(book_ids)))\
+            .all()
