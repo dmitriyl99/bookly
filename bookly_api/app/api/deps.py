f1750b8bb5be662f0a4c928115e0df44833a14f4
dmitriyl99 <dmitriyl1899@gmail.com>
2021-10-24 07:47:07 +0500

    rec sys in deps

--- bookly_api/app/api/deps.py
+++ bookly_api/app/api/deps.py
@@ -5,7 +5,8 @@ from fastapi import Depends
 from sqlalchemy.orm import Session
 
 from app.data.database import SessionLocal
-from app.data.repositories.readers_repository import ReadersRepository
+from app.data.repositories import BooksRepository, CirculationsRepository, ReadersRepository
+from app.core.recommendation import RecommendationSystem
 
 
 def get_db() -> Generator:
@@ -18,3 +19,19 @@ def get_db() -> Generator:
 
 def get_readers_repository(db: Session = Depends(get_db)) -> ReadersRepository:
     return ReadersRepository(db)
+
+
+def get_books_repository(db: Session = Depends(get_db)) -> BooksRepository:
+    return BooksRepository(db)
+
+
+def get_circulations_repository(db: Session = Depends(get_db)) -> CirculationsRepository:
+    return CirculationsRepository(db)
+
+
+def get_recommendation_service(readers_repository: ReadersRepository = Depends(get_readers_repository),
+                               books_repository: BooksRepository = Depends(get_books_repository),
+                               circulations_repository: CirculationsRepository = Depends(get_circulations_repository)):
+    return RecommendationSystem(readers=readers_repository,
+                                books=books_repository,
+                                circulations=circulations_repository)
